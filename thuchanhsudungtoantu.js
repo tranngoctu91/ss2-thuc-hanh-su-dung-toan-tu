3b0862295fd679e96807d56ccae02c543813965f
tran ngoc tu <ngoctu17101991@gmail.com>
2019-06-25 14:06:02 +0700

    the first time

---
+++ thuchanhsudungtoantu.js
@@ -0,0 +1,4 @@
+let inputWidth = parseInt(prompt("Enter the width"));
+let inputHeight = parseInt(prompt("Enter the height"));
+let area = inputHeight * inputWidth;
+document.write("The area of the rectangular is " + area);
